e70c288aadcfd42f1cf5793812bf5a05b507a17c
Alberto Benatti <albi.benni8@gmail.com>
2023-08-20 17:24:36 +0200

    action and description

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,16 @@
 
 import { Command } from "commander";
 
+// program declaration
+
 const program = new Command();
+
+// add action to the CLI
+program
+  .action(() => {
+    console.log("Hello, world neww!");
+  })
+  .description("Say hello to the world");
+
+// execute command
+program.parse(process.argv);
