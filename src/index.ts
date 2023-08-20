d311b551f612a79ed9647a4fca9a8eef8b3696a4
Alberto Benatti <albi.benni8@gmail.com>
2023-08-20 15:25:19 +0200

    working with npm i g

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
-const hello = (message: string) => {};
+const hello = (message: string) => {
+  console.log("🚀 ~ file: index.ts:4 ~ hello ~ message:", message);
+};
 
 console.log("wow");
