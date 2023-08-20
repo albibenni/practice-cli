0e09d013a1c591317599bd8bdb940629dc810f81
Alberto Benatti <albi.benni8@gmail.com>
2023-08-20 17:29:50 +0200

    with arguments printed out

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,11 @@ const program = new Command();
 
 // add action to the CLI
 program
-  .action(() => {
-    console.log("Hello, world neww!");
+  .argument("<string>", "string to print")
+  .action((message: string) => {
+    console.log(`Hello, world with ${message}!`);
   })
   .description("Say hello to the world");
 
-// execute command
+// execute command with given arguments
 program.parse(process.argv);
